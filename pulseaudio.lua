496fca9f298037d8648d519c8c74fb5bb4443fbb
Jason Fagnere <jason.fragnere@trivago.com>
2015-10-05 16:48:01 +0200

    Rewrote the module using awful and simpler PA commands

--- pulseaudio.lua
+++ pulseaudio.lua
@@ -1,65 +1,22 @@
-local io = io
-local math = math
 local tonumber = tonumber
-local tostring = tostring
-local string = string
+local awful = require("awful")
 
 module("pulseaudio")
 
 function volumeUp()
-    local step = 655 * 5
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local v = f:read()
-    f:close()
-    if v ~= nil then
-      local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
-      local newVolume = volume + step
-      if newVolume > 65536 then
-          newVolume = 65536
-      end
-      io.popen("pacmd set-sink-volume 0 "..newVolume)
-    end
+	if tonumber(awful.util.pread("pacmd list-sinks | awk -F'[[:blank:]:%]+' 'NF == 16 && $2 == \"volume\" { print $6; exit }'")) < 100 then
+		awful.util.spawn("pactl set-sink-volume 0 +5%", false)
+	end
 end
 
 function volumeDown()
-    local step = 655 * 5
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local v = f:read()
-    f:close()
-    if v ~= nil then
-      local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
-      local newVolume = volume - step
-      if newVolume < 0 then
-          newVolume = 0
-      end
-      io.popen("pacmd set-sink-volume 0 "..newVolume)
-    end
+	awful.util.spawn("pactl set-sink-volume 0 -5%", false)
 end
 
 function volumeMute()
-    local g = io.popen("pacmd dump |grep set-sink-mute")
-    local mute = g:read()
-    if string.find(mute, "no") then
-        io.popen("pacmd set-sink-mute 0 yes")
-    else
-        io.popen("pacmd set-sink-mute 0 no")
-    end
-    g:close()
+	awful.util.spawn("pactl set-sink-mute 0 toggle", false)
 end
 
 function volumeInfo()
-    volmin = 0
-    volmax = 65536
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local g = io.popen("pacmd dump |grep set-sink-mute")
-    local v = f:read()
-    local mute = g:read()
-    if mute ~= nil and string.find(mute, "no") then
-        volume = math.floor(tonumber(string.sub(v, string.find(v, 'x')-1)) * 100 / volmax).." %"
-    else
-        volume = "✕"
-    end
-    f:close()
-    g:close()
-    return "𝅘𝅥𝅮  "..volume
+	return awful.util.pread("pacmd list-sinks | awk '/muted: /{ print $2 }'") == "yes" and "00%" or awful.util.pread("pacmd list-sinks | awk -F'[[:blank:]:]+' 'NF == 16 && $2 == \"volume\" { print $6; exit }'")
 end
