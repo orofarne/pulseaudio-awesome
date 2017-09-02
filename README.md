987f95f9ed227ae8da08f5b9065d57783c9360d3
Bo Lorentsen <bl@lue.dk>
2017-09-02 23:55:35 +0200

    add async and default sink handling

--- README.md
+++ README.md
@@ -3,9 +3,16 @@ About
 
 Based on code from [awesome wiki](http://awesome.naquadah.org/wiki/Roultabie_volume_widget_for_PulseAudio)
 
+On startup we now detect the default pulse audio sink, and use this for all other operations. (we may dream of making a selection one day).
+
+All operations are now async, and uses ``awful.spawn`` famely as recommended, to minimize lagging.
+
+We assume pango support (markup) in text widget, for simple markup.
+
 Usage
 =====
 
+```lua
     require("pulseaudio")
 
     ...
@@ -16,16 +23,45 @@ Usage
         align = "right"
     })
     
+    -- common async functions, that also maintain widget
+    local function paVolumeUp()
+        pulseaudio.volumeUp(function()
+            pulseaudio.volumeInfo( function( info )
+                volumewidget.markup = info
+            end)
+        end)
+    end
+
+    local function paVolumeDown()
+        pulseaudio.volumeDown(function()
+            pulseaudio.volumeInfo( function( info )
+                volumewidget.markup = info
+            end)
+        end)
+    end
+
+    local function paVolumeMute()
+        pulseaudio.volumeMute(function()
+            pulseaudio.volumeInfo(function( info )
+                volumewidget.markup = info
+            end)
+        end)
+    end
+
     -- Optionally enable mousewheel support
     volumewidget:buttons(awful.util.table.join(
-      awful.button({ }, 4, function() pulseaudio.volumeUp(); volumewidget.text = pulseaudio.volumeInfo() end),
-      awful.button({ }, 5, function() pulseaudio.volumeDown(); volumewidget.text = pulseaudio.volumeInfo() end)
+      awful.button({ }, 4, paVolumeUp ),
+      awful.button({ }, 5, paVolumeDown )
     ))
     -- Thanks to elementalvoid
     
     volumewidget.text = pulseaudio.volumeInfo()
     volumetimer = timer({ timeout = 30 })
-    volumetimer:add_signal("timeout", function() volumewidget.text = pulseaudio.volumeInfo() end)
+    volumetimer:add_signal("timeout", function() 
+         pulseaudio.volumeInfo(function( info )
+            volumewidget.markup = info
+         end) 
+    end)
     volumetimer:start()
 
     ...
@@ -47,10 +83,10 @@ Usage
     globalkeys = awful.util.table.join(globalkeys,
         ...
 
-        awful.key({}, "XF86AudioMute", function() pulseaudio.volumeMute(); volumewidget.text = pulseaudio.volumeInfo() end),
-        awful.key({}, "XF86AudioLowerVolume", function() pulseaudio.volumeDown(); volumewidget.text = pulseaudio.volumeInfo() end),
-        awful.key({}, "XF86AudioRaiseVolume", function() pulseaudio.volumeUp(); volumewidget.text = pulseaudio.volumeInfo() end),
+        awful.key({}, "XF86AudioMute", paVolumeMute),
+        awful.key({}, "XF86AudioLowerVolume", paVolumeDown),
+        awful.key({}, "XF86AudioRaiseVolume", paVolumeUp),
 
         ...
     )
-
+```
--- pulseaudio.lua
+++ pulseaudio.lua
@@ -1,65 +1,89 @@
-local io = io
-local math = math
+-- handle all pulse audio calles async, and works only on the the default sink pulse reports
+-- heavyly based on the work done at https://github.com/orofarne/pulseaudio-awesome
+
+local awful = require( 'awful' )
+local gears = require( 'gears' )
+local string = require 'string'
+local math = require 'math'
 local tonumber = tonumber
-local tostring = tostring
-local string = string
 
-module("pulseaudio")
+local log = gears.debug.dump
 
-function volumeUp()
+module( 'pulseaudio' )
+
+-- Get default sink
+local default_sink = '0'
+awful.spawn.easy_async_with_shell('pacmd dump |grep set-default-sink', function( def_sink )
+  default_sink = string.match( def_sink, '^set%-default%-sink ([^$]+)' )
+  log( 'found default sink named : ' .. default_sink )
+end)
+
+function volumeUp( cb )
     local step = 655 * 5
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local v = f:read()
-    f:close()
-    if v ~= nil then
-      local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
-      local newVolume = volume + step
-      if newVolume > 65536 then
+    awful.spawn.easy_async_with_shell('pacmd dump | grep set-sink-volume | grep ' .. default_sink, function(v)
+      if v ~= nil then
+        local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
+        local newVolume = volume + step
+        if newVolume > 65536 then
           newVolume = 65536
+        end
+        awful.spawn.easy_async('pacmd set-sink-volume ' .. default_sink .. ' '.. newVolume, function()
+          if cb ~= nil then
+            cb()
+          end
+        end)
       end
-      io.popen("pacmd set-sink-volume 0 "..newVolume)
-    end
+    end)
 end
 
-function volumeDown()
+function volumeDown( cb )
     local step = 655 * 5
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local v = f:read()
-    f:close()
-    if v ~= nil then
-      local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
-      local newVolume = volume - step
-      if newVolume < 0 then
-          newVolume = 0
+    awful.spawn.easy_async_with_shell('pacmd dump | grep set-sink-volume | grep ' .. default_sink, function(v)
+      if v ~= nil then
+        local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
+        local newVolume = volume - step
+        if newVolume < 0 then
+            newVolume = 0
+        end
+        awful.spawn.easy_async('pacmd set-sink-volume ' .. default_sink .. ' ' .. newVolume, function()
+          if cb ~= nil then
+            cb()
+          end
+        end)
       end
-      io.popen("pacmd set-sink-volume 0 "..newVolume)
-    end
+    end)
 end
 
-function volumeMute()
-    local g = io.popen("pacmd dump |grep set-sink-mute")
-    local mute = g:read()
-    if string.find(mute, "no") then
-        io.popen("pacmd set-sink-mute 0 yes")
-    else
-        io.popen("pacmd set-sink-mute 0 no")
-    end
-    g:close()
+function volumeMute( cb )
+    awful.spawn.easy_async_with_shell('pacmd dump | grep set-sink-mute | grep ' .. default_sink, function( mute )
+      local state = 'no'
+
+      if string.find(mute, "no") then
+          state = 'yes'
+      end
+      awful.spawn.easy_async('pacmd set-sink-mute ' .. default_sink .. ' ' .. state, function()
+        if cb ~= nil then
+            cb()
+        end
+      end)
+    end)
 end
 
-function volumeInfo()
+function volumeInfo( cb )
     volmin = 0
     volmax = 65536
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local g = io.popen("pacmd dump |grep set-sink-mute")
-    local v = f:read()
-    local mute = g:read()
-    if mute ~= nil and string.find(mute, "no") then
-        volume = math.floor(tonumber(string.sub(v, string.find(v, 'x')-1)) * 100 / volmax).." %"
-    else
-        volume = "✕"
+
+    local fmt = function( volume )
+      return ' 𝅘𝅥𝅮  <span weight="bold">'..volume..'</span> '
     end
-    f:close()
-    g:close()
-    return "𝅘𝅥𝅮  "..volume
+
+    awful.spawn.easy_async_with_shell('pacmd dump |grep set-sink-mute | grep ' .. default_sink, function( mute )
+      if string.find(mute, "no") then
+          awful.spawn.easy_async_with_shell('pacmd dump | grep set-sink-volume | grep ' .. default_sink, function( v )
+            cb(fmt( math.floor(tonumber(string.sub(v, string.find(v, 'x')-1)) * 100 / volmax)..'%'))
+          end)
+      else
+          cb(fmt( "✕" ))
+      end
+    end)
 end
