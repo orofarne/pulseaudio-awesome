90b7fcc1fd383b8294ec41b2ceaba86b61da5cde
sfinam <sfinam@gmail.com>
2018-12-05 23:36:50 +0100

    Use default sink for all operations
    This changes allow the controls to work on the currently chosen default sink (if you choose another sink at runtime, it will automagically work on a new one instead of the old one).
    
    I'm pretty bad at lua, I just quickly made this for myself and decided to share. Please tell if there are any issues with this so I can fix them in my local config =)

--- pulseaudio.lua
+++ pulseaudio.lua
@@ -6,52 +6,63 @@ local string = string
 
 module("pulseaudio")
 
+function getSink()
+    local default_sink = io.popen("pacmd dump |grep set-default-sink")
+    local default_sink_string = default_sink:read()
+    local sink = string.sub(default_sink_string, string.find(default_sink_string, ' ') + 1)
+    return sink
+end
+
 function volumeUp()
+    local sink = getSink()
     local step = 655 * 5
-    local f = io.popen("pacmd dump |grep set-sink-volume")
+    local f = io.popen("pacmd dump |grep set-sink-volume | grep " .. sink)
     local v = f:read()
     f:close()
     if v ~= nil then
-      local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
-      local newVolume = volume + step
-      if newVolume > 65536 then
-          newVolume = 65536
-      end
-      io.popen("pacmd set-sink-volume 0 "..newVolume)
+        local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
+        local newVolume = volume + step
+        if newVolume > 65536 then
+            newVolume = 65536
+        end
+        io.popen("pacmd set-sink-volume " .. sink .. " " .. newVolume)
     end
 end
 
 function volumeDown()
+    local sink = getSink()
     local step = 655 * 5
-    local f = io.popen("pacmd dump |grep set-sink-volume")
+    local f = io.popen("pacmd dump |grep set-sink-volume | grep " .. sink)
     local v = f:read()
     f:close()
     if v ~= nil then
-      local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
-      local newVolume = volume - step
-      if newVolume < 0 then
-          newVolume = 0
-      end
-      io.popen("pacmd set-sink-volume 0 "..newVolume)
+        local volume = tonumber(string.sub(v, string.find(v, 'x') - 1))
+        local newVolume = volume - step
+        if newVolume < 0 then
+            newVolume = 0
+        end
+        io.popen("pacmd set-sink-volume " .. sink .. " " .. newVolume)
     end
 end
 
 function volumeMute()
-    local g = io.popen("pacmd dump |grep set-sink-mute")
+    local sink = getSink()
+    local g = io.popen("pacmd dump |grep set-sink-mute | grep " .. sink)
     local mute = g:read()
     if string.find(mute, "no") then
-        io.popen("pacmd set-sink-mute 0 yes")
+        io.popen("pacmd set-sink-mute " .. sink .. " yes")
     else
-        io.popen("pacmd set-sink-mute 0 no")
+        io.popen("pacmd set-sink-mute " .. sink .. " no")
     end
     g:close()
 end
 
 function volumeInfo()
+    local sink = getSink()
     volmin = 0
     volmax = 65536
-    local f = io.popen("pacmd dump |grep set-sink-volume")
-    local g = io.popen("pacmd dump |grep set-sink-mute")
+    local f = io.popen("pacmd dump |grep set-sink-volume | grep " .. sink)
+    local g = io.popen("pacmd dump |grep set-sink-mute | grep " .. sink)
     local v = f:read()
     local mute = g:read()
     if mute ~= nil and string.find(mute, "no") then
@@ -61,5 +72,5 @@ function volumeInfo()
     end
     f:close()
     g:close()
-    return "𝅘𝅥𝅮  "..volume
+    return "𝅘𝅥𝅮 "..volume
 end
